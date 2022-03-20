ed745056da6cfaf4e66c2c42b9b1efb612a778e0
HidemaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2022-03-20 11:46:32 +0900

    ubuntuにNeovimとvim-plugとcoc.nvimをインストール

--- install-neovim-vimPlug-deb.sh
+++ install-neovim-vimPlug-deb.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 sudo apt update
 sudo apt inatall -y neovim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
