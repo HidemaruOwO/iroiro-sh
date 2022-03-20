5c051fcb17a1bea5caef402403b96523b9035427
HidemaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2022-03-20 11:44:05 +0900

    archにNeovimとvim-plugとcoc.nvimをインストール

---
+++ install-neovim-vimPlug-arch.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo pacman -Syy
+sudo pacman -S --noconfirm neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cd ~
+mkdir .config
+cd .config
+mkdir nvim
+cd nvim
+wget https://raw.githubusercontent.com/HidemaruOwO/iroiro-sh/cdn/init.vim
+cd ~
+echo Run :PlugInstall on NeoVim
