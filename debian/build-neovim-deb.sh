98974c1a27cd1ae6c4b00750071eb3d9ad9fa3c9
HidemaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2022-03-31 21:19:35 +0900

    UbuntuでNeovimをビルドする

---
+++ debian/build-neovim-deb.sh
@@ -0,0 +1,7 @@
+cd /usr/local/src
+sudo git clone https://github.com/neovim/neovim.git
+cd neovim
+sudo apt update
+sudo apt install -y cmake automake libtool libtool-bin
+sudo make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
